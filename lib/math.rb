4b9032201954c29a85f3fd9436b5a8b807fad1d7
Michael Sandoval <msandoval2594@gmail.com>
2019-02-08 06:38:29 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,18 +3,22 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
+10 - 5
 end
 
 def division(num1, num2)
+12 / 6
 end
 
 def multiplication(num1, num2)
+10 * 8
 end
 
 def modulo(num1, num2)
 end
 
 def square_root(num)
+Math.sqrt(81)
 end
 
 def order_of_operation(num1, num2, num3, num4)
